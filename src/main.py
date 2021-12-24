1f999e4ed3cb96ba862db6ef492b5b811094fb02
honoka <honokanada.130@gmail.com>
2021-12-24 11:06:55 +0900

    Reply the answer of the last quiz before tweeting a new quiz
    Amend main.py and tweet.py so that it will reply the answer to the last tweet. The answer and tweet id replying to are loaded from a json.

--- src/main.py
+++ src/main.py
@@ -11,8 +11,16 @@ QUIZ_GENERATE_TRIALS = 15
 
 logger = log.get_logger(__name__, "log/test.log")
 
-if __name__ == '__main__':
 
+def reply_answer(client: tweepy.Client):
+    tweet_id, answer = tweet.load_tweet('data/prev_tweet.json')
+    reply_id = tweet.reply_answer(client, tweet_id, answer)
+    if reply_id == -1:
+        return 1
+    return 0
+
+
+def generate_and_tweet_quiz(client: tweepy.Client):
     with requests.Session() as session:
         page_ids = wikipedia.get_most_viewed_page_ids(session)
         shuffle(page_ids)
@@ -27,6 +35,17 @@ if __name__ == '__main__':
             logger.error("A valid quiz was not generated (trial = {0}).".format(
                 QUIZ_GENERATE_TRIALS))
 
+    tweet_id = tweet.tweet(client, generated_quiz)
+    if tweet_id == -1:
+        # Tweet failed.
+        return 1
+
+    tweet.save_tweet('data/prev_tweet.json', tweet_id, generated_quiz.answer)
+    return 0
+
+
+if __name__ == '__main__':
+
     client = tweepy.Client(
         bearer_token=twitter_api_tokens.BEARER_TOKEN,
         consumer_key=twitter_api_tokens.API_KEY,
@@ -34,10 +53,5 @@ if __name__ == '__main__':
         access_token=twitter_api_tokens.ACCESS_TOKEN,
         access_token_secret=twitter_api_tokens.ACCESS_TOKEN_SECRET)
 
-    tweet_id = tweet.tweet(client, generated_quiz)
-    if tweet_id == -1:
-        # Tweet failed.
-        exit(1)
-
-    tweet.save_tweet('data/prev_tweet.json', tweet_id, generated_quiz.answer)
-    exit(0)
+    reply_answer(client)
+    generate_and_tweet_quiz(client)
--- src/tweet.py
+++ src/tweet.py
@@ -20,6 +20,35 @@ def save_tweet(file, tweet_id, answer):
     logger.info("Saved the tweet data to " + file)
 
 
+def load_tweet(file):
+    # Loads tweet id and the answer from json
+    with open(file, encoding='UTF-8') as f:
+        data = json.load(f)
+    return data['tweet_id'], data['answer']
+
+
+def get_answer_tweet_text(answer):
+    return '答え: ' + answer
+
+
+def reply_answer(client: tweepy.Client, tweet_id, answer):
+    response = client.create_tweet(
+        in_reply_to_tweet_id=tweet_id,
+        text=get_answer_tweet_text(answer)
+    )
+
+    if len(response.errors) > 0:
+        for error in response.errors:
+            logger.error(
+                "Tweet error (in replying the answer): " + error_to_str(error))
+        return -1
+    else:
+        logger.info("Replied the answer successfully.")
+        tweet_id = int(response.data['id'])
+        logger.debug("Reply Tweet ID: {0}".format(tweet_id))
+        return tweet_id
+
+
 def tweet(client: tweepy.Client, quiz: quiz.Quiz):
     response = client.create_tweet(
         poll_duration_minutes=POLL_DURATION_MINUTES,
